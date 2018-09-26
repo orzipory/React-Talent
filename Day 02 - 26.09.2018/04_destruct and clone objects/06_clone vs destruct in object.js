dfc4dd36e2041f0dbe66f2a21331c8dbe0f526b8
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-09-26 16:52:40 +0300

    react

--- Day 02 - 26.09.2018/04_destruct and clone objects/06_clone vs destruct in object.js
+++ Day 02 - 26.09.2018/04_destruct and clone objects/06_clone vs destruct in object.js
@@ -1,7 +1,8 @@
+
+//------------------------OBJECT-----------------------
 let obj1 = {
     age: 11,
-    grade:100,
-    isMale:true
+    grade:100
 };
 
 //copy
@@ -10,3 +11,15 @@ let obj2={...obj1};
 //destruct
 let {age}={...obj1};
 console.log(age); //--> 11
+
+
+//------------------------ARRAY--------------------------
+
+let arr1=[1,2,3];
+
+//copy
+let arr2=[...arr1];
+
+//destruct
+let [a1,a2,a3]=arr1;
+
